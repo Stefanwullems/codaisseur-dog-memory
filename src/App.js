4a5e8cc72321326bd8f552c49080d72eb4433032
Prophetaa <klaux_15@hotmail.com>
2018-10-02 11:07:43 +0200

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
 import HeaderContainer from "./components/HeaderContainer";
+import FooterContainer from "./components/FooterContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HeaderContainer />
+
+
+        <FooterContainer></FooterContainer>
       </div>
     );
   }
