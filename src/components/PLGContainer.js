69d9fb7b2498450d47cc7e3751f026027f8568be
P4tr1ckk <p.r.buijs@hotmail.com>
2018-10-02 11:55:58 +0200

    route toegevoegd

---
+++ src/components/PLGContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+
+
+export default class PLGContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+  
+  export default BasicExample;
