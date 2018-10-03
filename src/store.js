49cab7abd52870afe5543bcc98ea8570cc6bd783
Stefanwullems <stefanwullems1998@gmail.com>
2018-10-03 10:55:00 +0200

    redux thunk integrated

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
+import ReduxThunk from "redux-thunk";
 
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-
+const enhancer = compose(
+  applyMiddleware(ReduxThunk),
+  devTools
+);
 
 export default createStore(rootReducer, enhancer);
